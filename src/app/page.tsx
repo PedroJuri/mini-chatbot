71510bd29713b950d5269b6a4564e7c086072982
PedroJuri <p3dr1n018@outlook.com>
2023-07-12 11:18:12 -0300

    feat: preparando estrutura e template base do CARD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,41 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen bg-slate-50 items-center justify-center">
-      <Card className="w-[440px] h-[700px]">
+      <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
         <CardHeader>
-          <CardTitle> Chat Ai</CardTitle>
+          <CardTitle> Chat AI</CardTitle>
           <CardDescription>Using Vercel SDK to cretate a chat bot.</CardDescription>
         </CardHeader>
-        <CardContent>
-          <p>Mensagens</p>
+        <CardContent className="space-y-4">
+          <div className="flex gap-3 text-slate-600 text-sm">
+            <Avatar>
+              <AvatarFallback>PH</AvatarFallback>
+              <AvatarImage src="https://github.com/PedroJuri.png"/>
+            </Avatar>
+            <p className="leading-relaxed">
+              <span className="block font-bold text-slate-700">Me:</span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem non, quam illo sit temporibus fugiat eius nostrum illum architecto nobis repudiandae, a corrupti rem itaque, ex ut necessitatibus! Incidunt, laboriosam.
+            </p>
+          </div>
+          <div className="flex gap-3 text-slate-600 text-sm">
+            <Avatar>
+              <AvatarFallback>PH</AvatarFallback>
+              <AvatarImage src="https://github.com/github.png"/>
+            </Avatar>
+            <p className="leading-relaxed">
+              <span className="block font-bold text-slate-700">AI:</span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem non, quam illo sit temporibus fugiat eius nostrum illum architecto nobis repudiandae, a corrupti rem itaque, ex ut necessitatibus! Incidunt, laboriosam.
+            </p>
+          </div>
         </CardContent>
-        <CardFooter>
-          Form
+        <CardFooter className="space-x-2">
+          <Input placeholder="How can I help you"/>
+          <Button type="submit">Send</Button>
         </CardFooter>
       </Card>
     </div>
