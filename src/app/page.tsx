2f3873401ad52150a976f84e1f1e2006b5aa31fe
PedroJuri <p3dr1n018@outlook.com>
2023-07-12 10:54:52 -0300

    feat: incluindo Shadcn/UI, com cards para inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+      <Card className="w-[440px] h-[700px]">
+        <CardHeader>
+          <CardTitle> Chat Ai</CardTitle>
+          <CardDescription>Using Vercel SDK to cretate a chat bot.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Mensagens</p>
+        </CardContent>
+        <CardFooter>
+          Form
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
