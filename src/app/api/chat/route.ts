00ddc51528be78dd190877de79b6fc4284acbeec
Pedro H. Santos <p3dr1n018@outlook.com>
2023-07-18 09:59:22 -0300

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -8,9 +8,9 @@ const openai = new OpenAIApi(config)
 
 export const runtime = 'edge'
  
-export async function POST(req: Request) {
+export async function get(req: Request) {
   const { messages } = await req.json()
- 
+
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     stream: true,
