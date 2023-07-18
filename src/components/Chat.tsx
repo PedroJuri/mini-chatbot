8b549722f1e06e009b1f89ea89074516d540215b
Pedro H. Santos <p3dr1n018@outlook.com>
2023-07-18 14:25:52 -0300

    Update Chat.tsx

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,56 +1,55 @@
 'use client'
 
-import { useState, ChangeEvent, FormEvent } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { useChat } from 'ai/react'
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-interface Message {
-  id: number;
-  content: string;
-  }
-
-export function Chat() {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [input, setInput] = useState('');
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  };
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const response = await fetch('/api/chat', {
-      method: 'POST',
-      body: JSON.stringify({ messages, input })
-    });
+export function Chat(){
+    const {messages, input, handleInputChange, handleSubmit} = useChat({
+        api: '/api/chat'
+    })
     
-    const responseData = await response.json();
-  };
-
-  return (
-    <Card className="w-[440px]">
-      <CardHeader>
-        <CardTitle> Chat AI</CardTitle>
-        <CardDescription>Using Vercel SDK to create a chat bot.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ScrollArea className="h-[600px] w-full pr-4">
-          {messages.map((message) => (
-            <div key={message.id}>
-              {/* Renderizar o conteúdo da mensagem aqui */}
-            </div>
-          ))}
-        </ScrollArea>
-      </CardContent>
-      <CardFooter>
-        <form className="w-full flex gap-2" onSubmit={handleSubmit}>
-          <Input placeholder="How can I help you?" value={input} onChange={handleInputChange} />
-          <Button type="submit">Send</Button>
-        </form>
-      </CardFooter>
-    </Card>
-  );
+    return(
+        <Card className="w-[440px]">
+        <CardHeader>
+          <CardTitle> Chat AI</CardTitle>
+          <CardDescription>Using Vercel SDK to cretate a chat bot.</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <ScrollArea className="h-[600px] w-full pr-4">
+                {messages.map(message => {
+                    return(
+                    <div key={message.id} className="flex gap-3 text-slate-600 text-sm mb-4">
+                        {message.role === 'user' && (
+                            <Avatar>
+                                <AvatarFallback>PH</AvatarFallback>
+                                <AvatarImage src="https://github.com/PedroJuri.png"/>
+                            </Avatar>
+                        )}
+                        {message.role === 'assistant' && (
+                            <Avatar>
+                                <AvatarFallback>OAI</AvatarFallback>
+                                <AvatarImage src="https://github.com/openai.png"/>
+                            </Avatar>
+                        )}
+                        <p className="leading-relaxed">
+                            <span className="block font-bold text-slate-700">{message.role === 'user' ? 'Me:' : 'AI:'}</span>
+                            {message.content}
+                        </p>
+                    </div>
+                    )
+                })}
+            </ScrollArea>
+        </CardContent>
+        <CardFooter>
+            <form className="w-full flex gap-2" onSubmit={handleSubmit}>
+                <Input placeholder="How can I help you?" value={input} onChange={handleInputChange}/>
+                <Button type="submit">Send</Button>
+            </form>
+        </CardFooter>
+      </Card>
+    )
 }
